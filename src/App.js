2f533bb00430e31787e757cd4cd57941fecb2dfa
“Aravind <aravindsnkr2@gmail.com>
2024-03-03 13:23:36 +0530

    Login, Signup, Homepage templates added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import LoginSignup from './components/LoginSignup/LoginSignup';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import SignUp from './components/SignUp/SignUp';
 
-function App() {
+const App = () => {
+  const isLoggedIn = Boolean(localStorage.getItem('loggedIn'));
+  console.log({ isLoggedIn });
 
-return (
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={isLoggedIn ? <Navigate replace to="/home" /> : <Navigate replace to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
+      </Routes>
+    </Router>
+  );
+};
 
-<div>
-  <LoginSignup />
-</div>
+const PrivateRoute = ({ children }) => {
+  if (!Boolean(localStorage.getItem('loggedIn'))) {
+    // Redirect them to the /login page, but save the current location they were
+    // trying to go to when they were redirected. This allows us to send them
+    // along to that page after they login, which is a nicer user experience
+    // than dropping them off on the home page.
+    return <Navigate to="/login" replace />;
+  }
 
-);
-
-}
+  return children;
+};
 
 export default App;
