8c32c2e1955af57a37cb2ba68ada2ce8fa976fe0
“Aravind <aravindsnkr2@gmail.com>
2024-03-05 10:30:55 +0530

    Fixed home page layout and node sass npm issue

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,18 +8,18 @@ const HomePage = () => {
   const medicines = [
     { name: 'Paracetamol', type: 'Tablet', dose: 500, unit: 'mg', dosage: 'Twice a day', timing: 'After meals' },
     { name: 'Benadryl', type: 'Syrup', dose: 10, unit: 'ml', dosage: 'Thrice a day', timing: 'After meals' },
+    { name: 'Asthalin', type: 'Vapour', dose: 15, unit: 'ml', dosage: 'Once a day', timing: 'After meals' },
   ];
 
   return (
     <div>
       <TopBar />
       <div className="container-fluid">
-        <div className="row">
+        <div className="container-row">
           <SideNav />
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <div className="row row-cols-1 row-cols-md-3 g-4 my-3">
+          <main className="px-md-4 card-container">
               {medicines.map((med, index) => (
-                <div className="col" key={index}>
+                <div className="card-item" key={index}>
                   <div className="card h-100">
                     <div className="card-body">
                       <h5 className="card-title">{med.name}</h5>
@@ -37,14 +37,13 @@ const HomePage = () => {
                   </div>
                 </div>
               ))}
-              <div className="col">
+              <div className="card-item">
                 <div className="card h-100 add-card">
                   <button className="add-card-button" aria-label="Add new medicine">
                     <Plus size={30} /> {/* Replace with your plus icon/image if not using Bootstrap icons */}
                   </button>
                 </div>
               </div>
-            </div>
           </main>
         </div>
       </div>
